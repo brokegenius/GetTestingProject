e6572f88bdc92b28c778a8c414418f327d8d0676
Michael Swanson <mswanson127@gmail.com>
2023-11-21 21:50:33 -0800

    Initial commit

---
+++ src/com/coderscampus/GetTestingApp.java
@@ -0,0 +1,9 @@
+package com.coderscampus;
+
+public class GetTestingApp {
+
+	public static void main(String[] args) {
+		//this is a test of project creation
+		System.out.println("this is a test");
+	}
+}
